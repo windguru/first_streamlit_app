660da45897639d69c4fa35ded02874f2d8ba60b9
windguru <tim@bothner.no>
2022-09-15 15:11:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast FAvorites')
 
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale, spinach and rocket smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text('🥗 Kale, spinach and rocket smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Advokado toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
