3ba1387ac912a56445635cdbc802cf083d654ff5
windguru <tim@bothner.no>
2022-09-15 15:08:19 +0200

    Update streamlit_app.py
    added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, spinach and rocket smoothie')
+streamlit.text('Hard boiled free-range egg')
+
