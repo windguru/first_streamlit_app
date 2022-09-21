0026dad8f63184964ff3bee84662fab207fbac04
windguru <tim@bothner.no>
2022-09-21 17:58:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.write('The user entered ', add_my_fruit)
-my_data_rows.add(add_my_fruit)
+my_data_rows.append(add_my_fruit)
 
 
 
