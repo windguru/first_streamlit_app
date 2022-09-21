7b85e6760fe21946c1589cf68404a35cf15b70ec
windguru <tim@bothner.no>
2022-09-21 18:04:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,10 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
 my_data_rows.append(add_my_fruit)
+my_fruit_list.loc[fruits_selected]
 streamlit.write('The user entered ', add_my_fruit)
+
+streamlit.dataframe(my_data_rows)
 
 
 
